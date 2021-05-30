a9e1c5d6a3503f07c64917fb34dc4c7300f9cba3
reda <reda@riseup.net>
2021-05-30 20:29:34 +0200

    chore: clean files up

--- exectimes.sh
+++ exectimes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
   # Usage: first argument is the version of the project to be used, the second # argument is number of splits
-  for l in 1000 10000 100000 1000000 10000000 100000000 1000000000;
+  for l in 1000 10000 100000 1000000 10000000 100000000;
   do 
     echo "Generating a file with $l lines"; 
     ./bin/project -m generation -o ./tests/test_$l.txt -n $l
--- innerSort.sh
+++ innerSort.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
   # Usage: first argument is the program mode, second one is the number of splits.
   # PLEASE CHANGE THE SORTALGO YOU WANT TO BE TESTED BEFORE RUNNING THIS SCRIPT
-  for l in 1000 10000 100000 1000000 10000000 100000000 1000000000;
+  for l in 1000 10000 100000 1000000 10000000 100000000;
   do
     if [ $1 = "demoSort" ]; 
     then
     echo "Generating a file with $l lines..."; 
     ./bin/project -m generation -o ./tests/test_$l.txt -n $l
     echo "Sorting a file with $l lines using demoSort, $2 splits...";
-    /usr/bin/time -v  ./bin/project -m demoSort -i ./tests/test_$l.txt -o ./tests/test_$l.innersort.txt -k $2 &>> ./tests/results_V$1.txt;
+    /usr/bin/time -v  ./bin/project -m demoSort -i ./tests/test_$l.txt -o ./tests/test_$l.innersort.txt -k $2 &>> ./tests/results_$1.txt;
     echo "Cleaning up...";
     rm ./tests/test*;
     echo "Done.";
@@ -17,7 +17,7 @@
     echo "Generating a file with $l lines..."; 
     ./bin/project -m generation -o ./tests/test_$l.txt -n $l
     echo "Sorting a file with $l lines using the command "sort -g", $2 splits...";
-    /usr/bin/time -v sort -g ./tests/test_$l.txt -o ./tests/test_$l.innersort.txt -k $2 &>> ./tests/results_V$1.txt;
+    /usr/bin/time -v sort -g ./tests/test_$l.txt -o ./tests/test_$l.innersort.txt -k $2 &>> ./tests/results_$1.txt;
     echo "Cleaning up...";
     rm ./tests/test*;
     echo "Done.";
